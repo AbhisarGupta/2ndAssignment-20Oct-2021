23e81a910da762c033d4b5496b3cebb8573dd3fa
AbhisarGupta <92710116+AbhisarGupta@users.noreply.github.com>
2021-10-21 22:04:10 +0530

    Add files via upload

---
+++ StudentQ7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2
+{
+    class StudentQ7
+    {
+        static void Main(string[] args)
+        {
+            int RollNo = 10;
+            string name = "Rohan";
+            double MarksInScince = 50;
+            double MarksinMaths = 60;
+            double MarksinEng = 40;
+
+            double totalMarks = MarksinEng + MarksinMaths + MarksInScince;
+            
+
+            double maxMarksPerSubject = 100;
+            double percentage=((totalMarks)/(maxMarksPerSubject*3))*100;
+            Console.WriteLine("RollNo:"+RollNo);
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("totalmarks:\t"+totalMarks);
+            Console.WriteLine("percentage:\t"+percentage+"%");
+        }
+    }
+}
---
+++ q1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class q1
+    {
+        public double Interest(double bal)
+        {
+            bal += (bal * 1.5) / 100;
+            return bal;
+        }
+
+        static void Main(string[] args)
+        {
+            double originalBal = 1000;
+            q1 interest = new q1();
+            double balAfterInterest = interest.Interest(originalBal);
+            //Console.WriteLine(balAfterInterest);
+            int monthlyPayment = 100;
+            double balAfterPayment = balAfterInterest - monthlyPayment;
+            int countPayments = 0;
+
+            while (balAfterPayment >= 100)
+            {              
+                countPayments += 1;
+                Console.WriteLine("Balance:" + balAfterPayment + "\ttotal  Payments:" + countPayments*100);
+                double newInterest = interest.Interest(balAfterPayment);
+                //Console.WriteLine(newInterest);
+                
+                balAfterPayment = newInterest - monthlyPayment;
+                //Console.WriteLine(balAfterPayment);
+            }
+
+        }
+    }
+}
